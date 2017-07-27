0737bc7c50ab890a173c41bf6c8cea9a4a0181d9
taqwa19-meet <taqwa19@meet.mit.edu>
2017-07-27 14:09:54 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,18 @@
+c=0
+for number in range(1,100+1):
+    print(number)
+    c=c+number
+print(c)
+
+
+def add_numbers(start,end):
+    for number in range(333,777+1):
+        print(number)
+        c=c+number
+    print(c)
+    return c
+
+    
+    
+   
+            
